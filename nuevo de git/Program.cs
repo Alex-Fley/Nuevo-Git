a802b7b6f792cc863726c20942b576eb42ba5939
LAPTOP-JDLMEGBS\GMG <alexfley2019@gmail.com>
2022-09-10 14:54:30 -0600

    Agregando fucionalidad del producto

--- nuevo de git/Program.cs
+++ nuevo de git/Program.cs
@@ -1,1 +1,23 @@
 ﻿Console.WriteLine("Hello, World!");
+Console.WriteLine("Hola Bay");
+
+
+
+string producto = "";
+double precio = 0.0;
+int cantidad = 0;
+
+
+Console.WriteLine("ingrese su producto");
+
+producto = Console.ReadLine();
+
+
+Console.WriteLine("ingrese el precio");
+precio = double.Parse(Console.ReadLine());
+
+Console.WriteLine("Ingrese la cantidad");
+cantidad = int.Parse(Console.ReadLine());
+
+Console.WriteLine("el producto {0} tiene una inversion de {1}" ,producto, (double)precio*cantidad);
+Console.ReadKey(); 
